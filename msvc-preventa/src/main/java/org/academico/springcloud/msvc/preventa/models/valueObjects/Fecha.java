f113c59420a5d525959649c64364c3d5e1543d44
LEGION <dmirandaf22_2@unc.edu.pe>
2025-08-15 19:30:24 -0500

    ov modificados

--- msvc-preventa/src/main/java/org/academico/springcloud/msvc/preventa/models/valueObjects/Fecha.java
+++ msvc-preventa/src/main/java/org/academico/springcloud/msvc/preventa/models/valueObjects/Fecha.java
@@ -1,22 +1,81 @@
 package org.academico.springcloud.msvc.preventa.models.valueObjects;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Embeddable;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+
+@Embeddable
 public class Fecha {
     private Integer dia;
     private Integer mes;
     private Integer anio;
 
-    public Fecha() {}
+    protected Fecha() {
+    }
 
-    public Fecha(Integer dia, Integer mes, Integer anio) {
+    @JsonCreator
+    public Fecha(@JsonProperty("dia") int dia,
+                 @JsonProperty("mes") int mes,
+                 @JsonProperty("anio") int anio) {
+
+        if (mes < 1 || mes > 12) {
+            throw new IllegalArgumentException("Mes inválido: debe estar entre 1 y 12");
+        }
+        if (anio < 1900) {
+            throw new IllegalArgumentException("Año inválido: debe ser mayor a 1900");
+        }
+        if (dia < 1 || dia > 31) {
+            throw new IllegalArgumentException("Día inválido: debe estar entre 1 y 31");
+        }
+
+        try {
+            LocalDate.of(anio, mes, dia);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Fecha inválida: combinación día-mes-año incorrecta");
+        }
+
+        if (LocalDate.of(anio, mes, dia).isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("Fecha inválida: no puede ser en el pasado");
+        }
+
         this.dia = dia;
         this.mes = mes;
         this.anio = anio;
     }
 
-    public Integer getDia() { return dia; }
-    public void setDia(Integer dia) { this.dia = dia; }
-    public Integer getMes() { return mes; }
-    public void setMes(Integer mes) { this.mes = mes; }
-    public Integer getAnio() { return anio; }
-    public void setAnio(Integer anio) { this.anio = anio; }
+    public int getDia() {
+        return dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public int getAño() {
+        return anio;
+    }
+
+
+    @Override
+    public String toString() {
+        return dia + "/" + mes + "/" + anio;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Fecha)) return false;
+        Fecha fecha = (Fecha) obj;
+        return dia == fecha.dia && mes == fecha.mes && anio == fecha.anio;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(dia, mes, anio);
+    }
 }
